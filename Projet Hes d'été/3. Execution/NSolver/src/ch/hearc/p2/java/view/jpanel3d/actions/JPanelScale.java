14dd6f75069229bf71a2b070b6d9877217690695
Kevin Vulliemin <kevin.vulliemin@he-arc.ch>
2015-08-27 14:47:33 +0200

    Correction de l'affichage par étape. Reste à corriger les étapes (rajout de finale et origine)

--- Projet Hes d'été/3. Execution/NSolver/src/ch/hearc/p2/java/view/jpanel3d/actions/JPanelScale.java
+++ Projet Hes d'été/3. Execution/NSolver/src/ch/hearc/p2/java/view/jpanel3d/actions/JPanelScale.java
@@ -1,3 +1,4 @@
+
 package ch.hearc.p2.java.view.jpanel3d.actions;
 
 import java.awt.Color;
@@ -18,30 +19,24 @@ import javax.swing.event.ChangeListener;
 
 import ch.hearc.p2.java.view.jpanel3d.JPanel3D;
 
-public class JPanelScale extends JPanel {
+public class JPanelScale extends JPanel
+	{
 
-	public final static double[] scaleValues = { 0.001, 0.0013, 0.0016, 0.002,
-			0.0025, 0.0032, 0.004, 0.005, 0.0063, 0.008, 0.01, 0.013, 0.016,
-			0.02, 0.025, 0.032, 0.04, 0.05, 0.063, 0.08, 0.1, 0.13, 0.16, 0.2,
-			0.25, 0.32, 0.4, 0.5, 0.63, 0.8, 1.0, 1.3, 1.6, 2.0, 2.5, 3.2, 4.0,
-			5.0, 6.3, 8.0, 10.0, 13.0, 16.0, 20.0, 25, 32, 40, 50, 63, 80, 100,
-			130, 160, 200, 250, 320, 400, 500, 630, 800, 1000 };
+	public final static double[] scaleValues = { 0.001, 0.0013, 0.0016, 0.002, 0.0025, 0.0032, 0.004, 0.005, 0.0063, 0.008, 0.01, 0.013, 0.016, 0.02, 0.025, 0.032, 0.04, 0.05, 0.063, 0.08, 0.1, 0.13, 0.16, 0.2, 0.25, 0.32, 0.4, 0.5, 0.63, 0.8, 1.0, 1.3, 1.6, 2.0, 2.5, 3.2, 4.0, 5.0, 6.3, 8.0, 10.0,
+			13.0, 16.0, 20.0, 25, 32, 40, 50, 63, 80, 100, 130, 160, 200, 250, 320, 400, 500, 630, 800, 1000 };
 
-	public JPanelScale(Font font, Color titleColor, float scaleFactor,
-			JPanel3D jPanel3d) {
+	public JPanelScale(Font font, Color titleColor, float scaleFactor, final JPanel3D jPanel3d)
+		{
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-		TitledBorder tBorderScale = BorderFactory
-				.createTitledBorder("Echelle de la boîte");
+		TitledBorder tBorderScale = BorderFactory.createTitledBorder("Echelle de la boîte");
 		tBorderScale.setTitleFont(font);
 		tBorderScale.setTitleColor(titleColor);
 		// tBorderScale.setBorder(BorderFactory.createLineBorder(bgColor));
 
-		this.setBorder(BorderFactory.createCompoundBorder(
-				BorderFactory.createRaisedBevelBorder(), tBorderScale));
+		this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), tBorderScale));
 
-		final JSlider jSliderScale = new JSlider(-30, 30,
-				(int) (10 * Math.log10(scaleFactor)));
+		final JSlider jSliderScale = new JSlider(-30, 30, (int)(10 * Math.log10(scaleFactor)));
 		jSliderScale.setFont(font);
 		jSliderScale.setAlignmentX(Component.LEFT_ALIGNMENT);
 		jSliderScale.setMajorTickSpacing(10);
@@ -61,32 +56,37 @@ public class JPanelScale extends JPanel {
 		jSliderScale.setLabelTable(labelTable);
 		jSliderScale.setPaintLabels(true);
 
-		jSliderScale.addChangeListener(new ChangeListener() {
+		jSliderScale.addChangeListener(new ChangeListener()
+			{
 
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				// if (!jSliderScale.getValueIsAdjusting()) {
-				float newScaleFactor = 0f;
-				if (jSliderScale.getValue() >= 0) {
-					newScaleFactor = (float) Math.round(Math.pow(10,
-							jSliderScale.getValue() / 10.0));
-				} else {
-					newScaleFactor = (float) Math.pow(10,
-							jSliderScale.getValue() / 10.0);
-				}
-				jPanel3d.setScaleFactor(newScaleFactor);
-				
-				float space = 100f;
-				while(space >= newScaleFactor)
-					space /= 10f;
-				
-				jPanel3d.setSpace(space);
+				@Override
+				public void stateChanged(ChangeEvent e)
+					{
+					// if (!jSliderScale.getValueIsAdjusting()) {
+					float newScaleFactor = 0f;
+					if (jSliderScale.getValue() >= 0)
+						{
+						newScaleFactor = Math.round(Math.pow(10, jSliderScale.getValue() / 10.0));
+						}
+					else
+						{
+						newScaleFactor = (float)Math.pow(10, jSliderScale.getValue() / 10.0);
+						}
+					jPanel3d.setScaleFactor(newScaleFactor);
 
-				jPanel3d.rescale();
+					float space = 100f;
+					while(space >= newScaleFactor)
+						{
+							space /= 10f;
+							}
 
-				// }
-			}
-		});
+					jPanel3d.setSpace(space);
+
+					jPanel3d.rescale();
+
+					// }
+					}
+			});
 
 		JPanel jPanel = new JPanel();
 		jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
@@ -102,5 +102,5 @@ public class JPanelScale extends JPanel {
 		//
 
 		add(jPanel);
+		}
 	}
-}
--- Projet Hes d'été/3. Execution/NSolver/src/ch/hearc/p2/java/view/jpanel3d/actions/JPanelSelection.java
+++ Projet Hes d'été/3. Execution/NSolver/src/ch/hearc/p2/java/view/jpanel3d/actions/JPanelSelection.java
@@ -1,3 +1,4 @@
+
 package ch.hearc.p2.java.view.jpanel3d.actions;
 
 import java.awt.Color;
@@ -14,91 +15,102 @@ import javax.swing.border.TitledBorder;
 
 import ch.hearc.p2.java.view.jpanel3d.JPanel3D;
 
-public class JPanelSelection extends JPanel {
+public class JPanelSelection extends JPanel
+	{
 
-	public JPanelSelection(Font font, Color titleColor, double[][] matrixValues, JPanel3D jPanel3d) {
+	public JPanelSelection(Font font, Color titleColor, final double[][] matrixValues, final JPanel3D jPanel3d)
+		{
 		initCheckBoxes(matrixValues);
-		
-		
-		this.setLayout(new BoxLayout(this,
-				BoxLayout.Y_AXIS));
 
-		TitledBorder tBorderSelection = BorderFactory
-				.createTitledBorder(" Fonctions affichées");
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		TitledBorder tBorderSelection = BorderFactory.createTitledBorder(" Fonctions affichées");
 		tBorderSelection.setTitleFont(font);
 		tBorderSelection.setTitleColor(titleColor);
 		// tBorderProjection.setBorder(BorderFactory.createLineBorder(bgColor));
 
-		this.setBorder(BorderFactory.createCompoundBorder(
-				BorderFactory.createRaisedBevelBorder(), tBorderSelection));
+		this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), tBorderSelection));
 
-		ActionListener selectionListener = new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				JCheckBox src = (JCheckBox) event.getSource();
-				int index = 3;
-				if (matrixValues.length >= 3 && src == jCheckFct[2])
-					index = 2;
-				else if (matrixValues.length >= 2 && src == jCheckFct[1])
-					index = 1;
-				else if (src == jCheckFct[0])
-					index = 0;
+		ActionListener selectionListener = new ActionListener()
+			{
 
-				jPanel3d.displayPlan(src.isSelected(), index);
-			}
-		};
+				public void actionPerformed(ActionEvent event)
+					{
+					JCheckBox src = (JCheckBox)event.getSource();
+					int index = 3;
+					if (matrixValues.length >= 3 && src == jCheckFct[2])
+						{
+						index = 2;
+						}
+					else if (matrixValues.length >= 2 && src == jCheckFct[1])
+						{
+						index = 1;
+						}
+					else if (src == jCheckFct[0])
+						{
+						index = 0;
+						}
+
+					jPanel3d.displayPlan(src.isSelected(), index);
+					}
+			};
 
 		JPanel jPanelSelection = new JPanel();
-		jPanelSelection.setLayout(new BoxLayout(jPanelSelection,
-				BoxLayout.Y_AXIS));
+		jPanelSelection.setLayout(new BoxLayout(jPanelSelection, BoxLayout.Y_AXIS));
 		// jPanelSelection.setAlignmentY(Component.TOP_ALIGNMENT);
-		
-		
+
 		jPanelSelection.add(Box.createVerticalStrut(5));
 
-		for (int i = 0; i < jCheckFct.length; i++) {
+		for(int i = 0; i < jCheckFct.length; i++)
+			{
 			jCheckFct[i].setFont(font);
 			jCheckFct[i].addActionListener(selectionListener);
 			jCheckFct[i].setForeground(jPanel3d.getPlanColor(i));
 
 			jPanelSelection.add(jCheckFct[i]);
 			jPanelSelection.add(Box.createVerticalStrut(10));
-			
-		}
+
+			}
 		// jCheckFct[].setForeground(bgColor);
 		// jCheckFct[].setAlignmentX(Component.LEFT_ALIGNMENT);
 
 		this.add(Box.createVerticalGlue());
 		this.add(jPanelSelection);
 		this.add(Box.createVerticalGlue());
-	}
-	
-	public boolean isCheckBoxSelected(int index){
+		}
+
+	public boolean isCheckBoxSelected(int index)
+		{
 		return jCheckFct[index].isSelected();
-	}
-	
-	private void initCheckBoxes(double[][] matrixValues) {
+		}
+
+	private void initCheckBoxes(double[][] matrixValues)
+		{
 		int nbEqu = matrixValues.length;
-		int nbVar = matrixValues[0].length-1;
+		int nbVar = matrixValues[0].length - 1;
 		String[] fcts_str = new String[nbEqu];
 		jCheckFct = new JCheckBox[nbEqu];
-		
-		for (int i = 0; i < nbEqu; i++) {
-			fcts_str[i] = "";
-			for (int j = 0; j < nbVar; j++) {
-				if (matrixValues[i][j] > 0)
-					fcts_str[i] += " + " + matrixValues[i][j] + ""
-							+ (char) (120 + j);
-				else if (matrixValues[i][j] < 0)
-					fcts_str[i] += " - " + (-matrixValues[i][j]) + ""
-							+ (char) (120 + j);
 
-			}
-			fcts_str[i] = " " + fcts_str[i].substring(2) + " = "
-					+ matrixValues[i][nbVar];
+		for(int i = 0; i < nbEqu; i++)
+			{
+			fcts_str[i] = "";
+			for(int j = 0; j < nbVar; j++)
+				{
+				if (matrixValues[i][j] > 0)
+					{
+					fcts_str[i] += " + " + matrixValues[i][j] + "" + (char)(120 + j);
+					}
+				else if (matrixValues[i][j] < 0)
+					{
+					fcts_str[i] += " - " + (-matrixValues[i][j]) + "" + (char)(120 + j);
+					}
+
+				}
+			fcts_str[i] = " " + fcts_str[i].substring(2) + " = " + matrixValues[i][nbVar];
 			jCheckFct[i] = new JCheckBox(fcts_str[i], true);
+			}
+
 		}
-		
-	}
 
 	private JCheckBox[] jCheckFct;
-}
+	}
